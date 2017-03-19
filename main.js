2bcac730074eec4f7364cc2ac7ebc6f3c833f925
Juan Carlos García Martínez <juan.kklzone@gmail.com>
2017-03-18 19:48:37 -0600

    Basic UI working, missing the hierarchy parameter. Only working for Linux

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const { dialog, ipcMain, app, BrowserWindow } = require('electron')
+const { spawn } = require("child_process")
 
 const path = require('path')
 const url = require('url')
@@ -10,28 +8,55 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+ipcMain.on('showDialog', function(event, data) {
+    electron.dialog.showOpenDialog({
+        title: "Select the directory",
+        createDirectory: false,
+        properties: ["openDirectory"],
+        buttonLabel: "Select"
+    }, function(folderPath) {
+        event.returnValue = folderPath;
+    });
+});
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+//ipcMain executes the shrink binary for compressing the image
+//it must be called the async way
+ipcMain.on('startCompression', function(event, parameters) {
+    let { input, output, quality, workers } = parameters;
+    console.log("in", input, "out", output, "workers",
+        workers, "quality", quality);
+    let shrink = spawn("./assets/shrink_linux", ["-dir", input, "-output", output, "-quality", quality, "-workers", workers, "-hierarchy"]);
+    shrink.stdout.on("data", (output_info) => {
+        event.sender.send("output", output_info)
+    });
+    shrink.stderr.on("data", (output_info) => {
+        event.sender.send("output", output_info)
+    });
+    shrink.on("close", (output_info) => {
+        event.sender.send("output", "Everything done!")
+    });
+});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+    // Open the DevTools.
+    // maisnWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
 }
 
 // This method will be called when Electron has finished
@@ -40,21 +65,18 @@ function createWindow () {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,3 +1,47 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const {
+    ipcRenderer
+} = require('electron');
+
+let inputFolder = "";
+let outputFolder = "";
+
+let lblInputFolder = document.getElementById("lbl-sourceFolder");
+let btnInputDialog = document.getElementById("btn-selectSourceFolder");
+console.log(lblInputFolder, btnInputDialog);
+btnInputDialog.addEventListener('click', function() {
+    let result = ipcRenderer.sendSync('showDialog');
+    console.log("resultado", result);
+    lblInputFolder.value = result;
+    inputFolder = result;
+});
+
+let lblDestFolder = document.getElementById("lbl-destFolder");
+let btnDestDialog = document.getElementById("btn-selectDestFolder");
+btnDestDialog.addEventListener("click", function() {
+    let result = ipcRenderer.sendSync('showDialog');
+    console.log(result);
+    lblDestFolder.value = result;
+    outputFolder = result;
+})
+
+let btnOK = document.getElementById("btn-ok");
+let resultInfo = document.getElementById("txt-result");
+
+ipcRenderer.on("output", (event, output) => {
+    resultInfo.value += output;
+});
+
+btnOK.addEventListener("click", function() {
+    resultInfo.value = ""
+    ipcRenderer.send("startCompression", {
+        input: inputFolder,
+        output: outputFolder,
+        quality: document.getElementById("quality").value,
+        workers: document.getElementById("workers").value,
+    });
+    // let output = ipcRenderer.sendSync("startCompression", {
+    //     input: inputFolder,
+    //     output: outputFolder
+    // });
+    // resultInfo.value = output;
+});
